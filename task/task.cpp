d3ec682a01f50f48956572b07bb9e0c682bbaa1d
Brendan Fay <brendan.fay@columbia.edu>
2023-01-22 22:29:50 -0500

    .

--- task/task.cpp
+++ task/task.cpp
@@ -6,12 +6,10 @@ int main() {
 	
 	cin.tie(nullptr)->sync_with_stdio(false);
 	int t; cin >> t;
+	
 	while(t--) {
-		int n; cin >> n;
-		vector<int> a(n);
-		for (int i = 0; i < n; i++) cin >> a[i];
-		for (auto c : a) cout << c << " ";
-		cout << "\n";
+	
 	}
+	
 	return 0;
 }	
