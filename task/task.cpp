0c901b38a8dde068dbb5bcb8c43d2c9c7542e07a
brendan <brendan.fay@columbia.edu>
2023-02-06 21:01:41 -0500

    modified task

--- task/task.cpp
+++ task/task.cpp
@@ -1,7 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  task.cpp
+ *
+ *    Description:  My competitive programming file 
+ *
+ *        Version:  1.0
+ *        Created:  02/06/2023 08:46:59 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  brendan
+ *   Organization:  Columbia
+ *
+ * =====================================================================================
+ */
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
 
-signed main() {
-	
+signed main()
+{
+
 }
+
