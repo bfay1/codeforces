e40b87bc256630d0430faf90a48be534b4b1d2f0
brendan <brendan.fay@columbia.edu>
2023-03-25 08:16:41 -0400

    commit

--- task.cpp
+++ task.cpp
@@ -1,12 +1,6 @@
 #include <bits/stdc++.h>
-using namespace std;
-#define int long long
 
-void solve();
-
-signed main()
+int main()
 {
-	cin.tie(nullptr)->sync_with_stdio(false);
-	int t; cin >> t;
-	while (t--) solve();
+
 }
