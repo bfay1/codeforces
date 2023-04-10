850e14502539a7eaf80235be63d867e0844f6f62
brendan <brendan.fay@columbia.edu>
2023-04-10 09:46:54 -0400

    did a problem

---
+++ codeforces/misc/ian-and-array-sorting.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve()
+{
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (auto& x : a)
+		cin >> x;
+	for (int i = 1; i < n - 1; i++) {
+		int d = a[i - 1] - a[i];
+		a[i] += d;
+		a[i + 1] += d;
+	}
+	if (a[n - 2] <= a[n - 1] || n % 2)
+		cout << "YES\n";
+	else
+		cout << "NO\n";
+}
+
+signed main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+}
